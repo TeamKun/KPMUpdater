7a58d46a6926be2574b1dc5715ddc5e34e12d830
Peyang <42040068+peyang-Celeron@users.noreply.github.com>
2021-05-28 18:46:12 +0900

    Initial Commit

---
+++ src/main/java/net/kunmc/lab/kpmupdater/KPMUpdater.java
@@ -0,0 +1,12 @@
+package net.kunmc.lab.kpmupdater;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class KPMUpdater extends JavaPlugin
+{
+
+    @Override
+    public void onEnable()
+    {
+    }
+}
